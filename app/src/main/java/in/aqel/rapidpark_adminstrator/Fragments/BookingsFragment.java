41ac003ba5d862fe7346dc346fb34acefa34d60c
Aqel Ahammad <aqel123@gmail.com>
2016-01-25 23:31:45 +0530

    Added icon admin

---
+++ app/src/main/java/in/aqel/rapidpark_adminstrator/Fragments/BookingsFragment.java
@@ -0,0 +1,34 @@
+package in.aqel.rapidpark_adminstrator.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import in.aqel.rapidpark_adminstrator.R;
+
+public class BookingsFragment extends Fragment {
+
+    public BookingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_bookings, container, false);
+
+
+        return view;
+    }
+
+
+}
